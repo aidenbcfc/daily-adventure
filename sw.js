2e1f98ae376c59490dfcf960b22a654d3d430219
aidenbcfc <aidenbcfc9@gmail.com>
2026-06-07 21:57:20 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "daily-adventure-v1";
+
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json"
+];
+
+// Install service worker + cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
